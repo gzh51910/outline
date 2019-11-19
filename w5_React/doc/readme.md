68bacebc9599094b97d28cc6275e2d7e09293e08
aaron <jeroo@qq.com>
2019-11-19 10:51:46 +0800

    day5-2a React JSX语法

---
+++ w5_React/doc/readme.md
@@ -0,0 +1,14 @@
+# React
+
+## day5-2
+* 依赖
+    * react
+    * react-dom 
+* React渲染
+    > ReactDOM.render(VNode,target)
+* 创建节点（VNode）
+    > React.createElement(type,[props],[...children])
+* 热更新（自动刷新浏览器）
+    > 原理：websocket
+* JSX
+    > 在js中编写HTML代码，在编译时webpack会通过特定的规则（loader）编译JSX代码到JS
---
+++ w5_React/myapp/src/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// let res = React.createElement('div',{id:'container'},'text');
+// let res2 = <div id="container">text</div>
+// console.log('res:',res,res2)
+
+let username = 'laoxie';
+let myStyle = {color:'#f00',fontSize:20}
+
+// 渲染方法render(content,target)
+ReactDOM.render(
+    // <div>Hello, World</div>
+    // React.createElement('div',null,'Hello World')
+    // <div id="container" class="home"><h1>Hello, World</h1><ul><li>data1</li><li>data2</li></ul></div>
+    // React.createElement(
+    //     'div',
+    //     {id:'container',className:'home'},
+    //     [
+    //         React.createElement('h1',null,'Hello World'),
+    //         React.createElement('ul',null,[
+    //             React.createElement('li',null,'data1'),
+    //             React.createElement('li',null,'data2'),
+    //             React.createElement('li',null,'data3')
+    //         ])
+    //     ]
+    // )
+
+    // JSX写法
+    <div id="container" className="home">
+        {
+            //<h1>Hello, {<strong>{username}</strong>}</h1>
+        }
+        <ul>
+            <li>data1</li>
+            <li>data2</li>
+        </ul>
+        <label htmlFor="username" style={myStyle}>用户名：</label>
+        <input id="username" autoFocus onKeyDown={()=>{console.log('keydown')}} />
+        <img/>
+    </div>
+    , document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
