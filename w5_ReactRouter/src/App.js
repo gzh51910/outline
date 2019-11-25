038acb1d3a9fa792fd16ccfa47a5e5e60eb31db1
aaron <jeroo@qq.com>
2019-11-25 09:44:46 +0800

    解决冲突

--- w5_ReactRouter/src/App.js
+++ w5_ReactRouter/src/App.js
@@ -1,6 +1,6 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
-import {Route,Redirect,Switch,Link,NavLink,withRouter} from 'react-router-dom';
+import { Route, Redirect, Switch, Link, NavLink, withRouter } from 'react-router-dom';
 
 // import Home from './pages/Home';
 // import Discover from './pages/List';
@@ -28,9 +28,9 @@ import Goods from '~/Goods';
 // import 'antd/es/icon/style/css'; // 加载 CSS
 
 // 基于babel-plugin-import的按需加载
-import {Menu, Icon} from 'antd';
+import { Menu, Icon } from 'antd';
 
-class App extends Component{
+class App extends Component {
     /* constructor(props){
         super(props)
 
@@ -60,10 +60,7 @@ class App extends Component{
         this.goto = this.goto.bind(this);
     }
 
-<<<<<<< HEAD
     // 添加原型方法
-=======
->>>>>>> 78d95a06972ddb0fe3166f5b04c71d9b0418414b
     goto(path){
         let {history} = this.props;
        console.log(this.props);
@@ -71,77 +68,51 @@ class App extends Component{
        history.push(path)
     } */
 
-<<<<<<< HEAD
     // 添加实例属性/方法
     state = {
-        currentPath:'/home',
-=======
-    state = {
->>>>>>> 78d95a06972ddb0fe3166f5b04c71d9b0418414b
-        menu:[
+        currentPath: '/home',
+        menu: [
             {
-                name:'home',
-                path:'/home',
-<<<<<<< HEAD
-                icon:'home',
-=======
->>>>>>> 78d95a06972ddb0fe3166f5b04c71d9b0418414b
-                text:'首页'
-            },{
-                name:'discover',
-                path:'/discover',
-<<<<<<< HEAD
-                icon:'compass',
-=======
->>>>>>> 78d95a06972ddb0fe3166f5b04c71d9b0418414b
-                text:'发现'
+                name: 'home',
+                path: '/home',
+                icon: 'home',
+                text: '首页'
+            }, {
+                name: 'discover',
+                path: '/discover',
+                icon: 'compass',
+                text: '发现'
             },
             {
-                name:'cart',
-                path:'/cart',
-<<<<<<< HEAD
-                icon:'shopping-cart',
-=======
->>>>>>> 78d95a06972ddb0fe3166f5b04c71d9b0418414b
-                text:'购物车'
-            },{
-                name:'mine',
-                path:'/mine',
-<<<<<<< HEAD
-                icon:'user',
-=======
->>>>>>> 78d95a06972ddb0fe3166f5b04c71d9b0418414b
-                text:'我的'
+                name: 'cart',
+                path: '/cart',
+                icon: 'shopping-cart',
+                text: '购物车'
+            }, {
+                name: 'mine',
+                path: '/mine',
+                icon: 'user',
+                text: '我的'
             },
         ]
     }
-<<<<<<< HEAD
-    goto = ({key:path})=>{
-        let {history} = this.props;
+    goto = ({ key: path }) => {
+        let { history } = this.props;
         this.setState({
-            currentPath:path
+            currentPath: path
         })
-       history.push(path)
+        history.push(path)
     }
-    componentDidMount(){
+    componentDidMount() {
         // console.log('props',this.props)
         this.setState({
-            currentPath:this.props.location.pathname
+            currentPath: this.props.location.pathname
         })
     }
-=======
-    goto = (path)=>{
-        let {history} = this.props;
-       console.log(this.props);
-
-       history.push(path)
-    }
->>>>>>> 78d95a06972ddb0fe3166f5b04c71d9b0418414b
-    render(){
+    render() {
         console.log(this)
         return (
             <div>
-<<<<<<< HEAD
                 {/* <ul>
                 {
                     this.state.menu.map(item=>{
@@ -151,56 +122,50 @@ class App extends Component{
                             activeStyle={{color:'#f00',fontWeight:'bold'}}
                             replace
                             >{item.text}</NavLink>
-=======
                 <ul>
                 {
                     this.state.menu.map(item=>{
                         return <li key={item.name} onClick={this.goto.bind(this,item.path)}>
-                            {/* <NavLink 
+                            <NavLink 
                             to={item.path}
                             activeStyle={{color:'#f00',fontWeight:'bold'}}
                             replace
-                            >{item.text}</NavLink> */}
->>>>>>> 78d95a06972ddb0fe3166f5b04c71d9b0418414b
+                            >{item.text}</NavLink> }
                             {item.text}
                         </li>
                     })
                 }
-<<<<<<< HEAD
                 </ul> */}
 
-                <Menu 
-                onClick={this.goto} 
-                selectedKeys={[this.state.currentPath]} 
-                mode="horizontal"
-                theme="dark"
+                <Menu
+                    onClick={this.goto}
+                    selectedKeys={[this.state.currentPath]}
+                    mode="horizontal"
+                    theme="dark"
                 >
-                {
-                    this.state.menu.map(item=>{
-                        return <Menu.Item key={item.path}>
-                            <Icon type={item.icon} />
-                            {item.text}
-                        </Menu.Item>
-                    })
-                }
+                    {
+                        this.state.menu.map(item => {
+                            return <Menu.Item key={item.path}>
+                                <Icon type={item.icon} />
+                                {item.text}
+                            </Menu.Item>
+                        })
+                    }
                 </Menu>
-=======
-                </ul>
-                
->>>>>>> 78d95a06972ddb0fe3166f5b04c71d9b0418414b
-                    <Switch>
-                        {/* <Route path="/" component={Home} exact/> */}
-                        <Route path="/home" component={Home}/>
-                        <Route path="/discover" component={Discover}/>
-                        <Route path="/cart" component={Cart}/>
-                        <Route path="/reg" component={Reg}/>
-                        <Route path="/login" component={Login}/>
-                        <Route path="/mine" component={Mine}/>
-                        <Route path="/goods/:id" component={Goods}/>
-                        <Route path="/notfound" render={()=><div>404页面</div>}/>
-                        <Redirect from="/" to="/home" exact/>
-                        <Redirect to="/notfound"/>
-                    </Switch>
+
+                <Switch>
+                    {/* <Route path="/" component={Home} exact/> */}
+                    <Route path="/home" component={Home} />
+                    <Route path="/discover" component={Discover} />
+                    <Route path="/cart" component={Cart} />
+                    <Route path="/reg" component={Reg} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/mine" component={Mine} />
+                    <Route path="/goods/:id" component={Goods} />
+                    <Route path="/notfound" render={() => <div>404页面</div>} />
+                    <Redirect from="/" to="/home" exact />
+                    <Redirect to="/notfound" />
+                </Switch>
             </div>
         )
     }
